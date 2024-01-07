23b773b2906b4ebb0e6edb82d59d36cdc8b47bfe
collins Boakye <colly0087@gmail.com>
2024-01-07 04:16:55 -0800

    test

--- dev/strings.c
+++ dev/strings.c
@@ -5,6 +5,7 @@
  * @s2: second string
  * Return: nothing
 */
+int _strcmp(const char *s1, const char *s2);
 int _strcmp(const char *s1, const char *s2)
 {
 	int x;
@@ -75,7 +76,7 @@ char *_strcpy(char *dest, char *src)
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
-	
+
 	return (dest);
 }
 
